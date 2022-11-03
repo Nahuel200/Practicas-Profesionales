0d76fb67355be4c4cb04799e716ec925b58404fb
Federico Pereyra <crearcuen15@gmail.com>
2022-11-03 20:44:42 -0300

    Alumno Class

---
+++ GestionPS/GestionPS/Entidades/Alumno.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+
+        private String Legajo;
+        private String Nombre;
+        private String Apellido;
+        private String Mail;
+
+
+        public Alumno()
+        {
+            this.Legajo = null;
+            this.Nombre = null;
+            this.Apellido = null;
+            this.Mail = null;
+        }
+
+        public Alumno(String _legajo, String _nombre, String _apellido , String _mail)
+        {
+            this.Legajo = _legajo;
+            this.Nombre = _nombre;
+            this.Apellido = _apellido;
+            this.Mail = _mail;
+
+        }
+
+        public String getLegajo()
+        {
+            return this.Legajo;
+
+        }
+
+        public void setLegajo(String _Legajo)
+        {
+            this.Legajo = _Legajo;
+
+
+        }
+
+        public String getNombre()
+        {
+            return this.Nombre;
+
+        }
+
+        public void setNombre(String _Nombre)
+        {
+            this.Nombre = _Nombre;
+
+
+        }
+
+        public String getApellido()
+        {
+            return this.Apellido;
+
+        }
+
+        public void setApellido(String _Apellido)
+        {
+            this.Apellido = _Apellido;
+
+
+        }
+
+        public String getMail()
+        {
+            return this.Mail;
+
+        }
+
+        public void setMail(String _Mail)
+        {
+            this.Mail = _Mail;
+
+
+        }
+
+
+    }
+}
---
+++ GestionPS/GestionPS/Negocio/AlumnoNegocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Negocio
+{
+    class AlumnoNegocio
+    {
+        public void AgregarAlumno()
+        {
+           
+
+           
+        }
+
+
+
+
+    }
+}
