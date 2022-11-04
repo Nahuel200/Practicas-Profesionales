beea5d91f275ce5d789020173da09cf1a0e59b4a
LGF798 <83374985+LGF798@users.noreply.github.com>
2022-11-04 09:38:31 -0300

    Creación de la conexion de base de datos
    en el dao se coloco el AccesoDatos.cs con la correspondiente conexión y se testeó, funciona sin problemas.
    Se creo también una entidad Alumno y un DaoAlumno incompleto que trae una tabla

---
+++ GestionPS/GestionPS/Dao/DaoAlumno.cs
@@ -0,0 +1,25 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    class DaoUsuario
+    {
+        AccesoDatos ad = new AccesoDatos();
+        public Alumno getAlumno(Alumno al)
+        {
+            DataTable tabla = ad.ObtenerTabla("alumnos", "SELECT legajo_alumnos, nombre_alumnos, apellido_alumnos, mail_alumnos FROM practica_supervisada.alumnos WHERE legajo_alumnos = ' " + al.Legajo1 + "'");
+
+            al.Legajo1 = tabla.Rows[0][0].ToString();
+            al.Nombre1 = tabla.Rows[0][1].ToString();
+            al.Apellido1 = tabla.Rows[0][2].ToString();
+            al.Mail1 = tabla.Rows[0][3].ToString();
+            return al;
+        }
+    }
+}
--- GestionPS/GestionPS/Entidades/Alumno.cs
+++ GestionPS/GestionPS/Entidades/Alumno.cs
@@ -10,76 +10,17 @@ namespace Entidades
         private String Apellido;
         private String Mail;
 
-
-        public Alumno()
+        public Alumno(string legajo, string nombre, string apellido, string mail)
         {
-            this.Legajo = null;
-            this.Nombre = null;
-            this.Apellido = null;
-            this.Mail = null;
+            Legajo = legajo;
+            Nombre = nombre;
+            Apellido = apellido;
+            Mail = mail;
         }
 
-        public Alumno(String _legajo, String _nombre, String _apellido , String _mail)
-        {
-            this.Legajo = _legajo;
-            this.Nombre = _nombre;
-            this.Apellido = _apellido;
-            this.Mail = _mail;
-
-        }
-
-        public String getLegajo()
-        {
-            return this.Legajo;
-
-        }
-
-        public void setLegajo(String _Legajo)
-        {
-            this.Legajo = _Legajo;
-
-
-        }
-
-        public String getNombre()
-        {
-            return this.Nombre;
-
-        }
-
-        public void setNombre(String _Nombre)
-        {
-            this.Nombre = _Nombre;
-
-
-        }
-
-        public String getApellido()
-        {
-            return this.Apellido;
-
-        }
-
-        public void setApellido(String _Apellido)
-        {
-            this.Apellido = _Apellido;
-
-
-        }
-
-        public String getMail()
-        {
-            return this.Mail;
-
-        }
-
-        public void setMail(String _Mail)
-        {
-            this.Mail = _Mail;
-
-
-        }
-
-
+        public string Legajo1 { get => Legajo; set => Legajo = value; }
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public string Apellido1 { get => Apellido; set => Apellido = value; }
+        public string Mail1 { get => Mail; set => Mail = value; }
     }
 }
